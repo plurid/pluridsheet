d69ad6c44472714dac35cfdebb01d8b791d6e945
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-11-01 19:09:17 +0200

    pluridsheet-react > setup: package

---
+++ packages/pluridsheet-react/.storybook/main.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+
+
+module.exports = {
+    stories: [
+        '../source/**/*.stories.@(js|jsx|ts|tsx)',
+    ],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+    ],
+    webpackFinal: async (config, { configType }) => {
+        config.resolve.modules = [
+            ...(config.resolve.modules || []),
+            path.resolve(__dirname, '../'),
+            path.resolve(__dirname, '../source'),
+        ];
+
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            "~data": path.resolve(__dirname, "../source/data"),
+            "~components": path.resolve(__dirname, "../source/components"),
+            "~utilities": path.resolve(__dirname, "../source/utilities"),
+        };
+
+        return config;
+    },
+};
--- packages/pluridsheet-react/source/components/Pluridsheet/__stories__/index.stories.tsx
+++ packages/pluridsheet-react/source/components/Pluridsheet/__stories__/index.stories.tsx
@@ -1,25 +1,14 @@
 // #region imports
     // #region libraries
     import React from 'react';
-
-    import themes from '@plurid/plurid-themes';
-
-    import { storiesOf } from '@storybook/react';
-    // import { action } from '@storybook/addon-actions';
     import {
-        withKnobs,
-        select,
-    } from '@storybook/addon-knobs';
+        ComponentStory,
+        ComponentMeta,
+    } from '@storybook/react';
     // #endregion libraries
 
 
     // #region external
-    import {
-        themeLabel,
-        themeOptions,
-        defaultThemeValue,
-    } from '~utilities/storybook';
-
     import Pluridsheet from '../';
     // #endregion external
 // #endregion imports
@@ -27,26 +16,14 @@
 
 
 // #region module
-storiesOf(
-    'Pluridsheet',
-    module,
-)
-.addDecorator(withKnobs)
-.add('Pluridsheet', () => {
-    const theme = select(themeLabel, themeOptions, defaultThemeValue);
+export default {
+    title: 'Pluridsheet',
+    component: Pluridsheet,
+} as ComponentMeta<typeof Pluridsheet>;
 
-    return (
-        <div
-            style={{
-                backgroundColor: 'black',
-                height: '800px',
-                padding: '2rem',
-            }}
-        >
-            <Pluridsheet
-                theme={themes[theme]}
-            />
-        </div>
-    );
-});
+
+const Template: ComponentStory<typeof Pluridsheet> = (args) => <Pluridsheet {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {};
 // #endregion module
